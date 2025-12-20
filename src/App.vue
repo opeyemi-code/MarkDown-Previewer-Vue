df67d04f54fa55f33840e9d98adc63eb77c1b010
Obatola Opeyemi <opeyemiobatola@gmail.com>
2025-12-20 04:34:50 +0100

    build(files): scalfold project for development

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const person = ref("John Doe");
+</script>
+
+<template>
+  <h1 class="text-red-400">Hello, {{ person }}</h1>
+  <p class="text-red-900">Testing something...</p>
+</template>
+
+<style scoped></style>
